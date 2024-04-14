faadd5cdcf6b3410b307dee93b80ca169c215d90
Kristine Bugasto <kaabugasto@gmail.com>
2024-04-15 01:05:14 +0800

    Update: Added Book Factory, Routes, Controllers, and Views

---
+++ database/factories/BookFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Book>
+ */
+class BookFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => fake()->unique()->id(),
+            'isbn' => fake()->unique()->isbn13(),
+            'title' => fake()->title(),
+            'author' => fake()->name(), 
+            'description' => fake()->paragraph(),
+            'date_published' => fake()->date(),
+        ];
+    }
+}
